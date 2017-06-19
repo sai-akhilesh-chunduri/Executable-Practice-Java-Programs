ba5bb9f115201951122f601ce789d26ed3a75382
valentine-vampire <akhilesh.hurricane@gmail.com>
2017-06-19 13:47:44 +0530

    sample.java

---
+++ JAVA/sample.java
@@ -0,0 +1,5 @@
+class sample{
+       public static void main(java.lang.String args[]){
+      System.out.println("This is just a sample program. I have created this to learn about how to push this program on to my github.There's no code present here");
+   }
+}
