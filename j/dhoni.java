6ce33f8ceb77c25a81404f1c3c0b2c3363c21819
valentine-vampire <akhilesh.hurricane@gmail.com>
2017-06-19 14:51:24 +0530

    dhoni.java

---
+++ j/dhoni.java
@@ -0,0 +1,6 @@
+//Ignore reading this program! It's a demo program I have created to know the working of GIT
+class dhoni{
+      public static void main(java.lang.String args[]){
+        System.out.println("Dhoni was a great captain fot Team INDIA and he is irreplacable");
+    }
+}
