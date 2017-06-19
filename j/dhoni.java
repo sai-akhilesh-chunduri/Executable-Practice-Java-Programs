97e83eb1389a1db85dd416d8f9f3a053174d54b3
valentine-vampire <akhilesh.hurricane@gmail.com>
2017-06-19 15:00:19 +0530

    Remove dhoni.java

--- j/dhoni.java
+++
@@ -1,6 +0,0 @@
-//Ignore reading this program! It's a demo program I have created to know the working of GIT
-class dhoni{
-      public static void main(java.lang.String args[]){
-        System.out.println("Dhoni was a great captain fot Team INDIA and he is irreplacable");
-    }
-}
